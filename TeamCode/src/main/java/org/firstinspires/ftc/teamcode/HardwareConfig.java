bdb76fa763b9f44ef3d13bb703b1daf3c7404f40
Brandon M <AzureDe;ta5356@gmail.com>
2018-09-27 18:59:28 -0700

    Updated variable names to be match new TeleOp code, names should be shorter and more descriptive.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfig.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,18 +12,18 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 
 public class HardwareConfig {
     /* Public OpMode members. */
-    public DcMotor frontRight = null;
-    public DcMotor frontLeft = null;
-    public DcMotor backRight = null;
-    public DcMotor backLeft = null;
-    public DcMotor rackpinion = null;
-    public Servo upleftClaw = null;
-    public Servo uprightClaw = null;
-    public Servo bottomleftClaw = null;
-    public Servo bottomrightClaw = null;
-    public static final double MID_SERVO = 0.5;
-    public Servo jewelhitter = null;
-    public ColorSensor colorSensor;
+    public DcMotor motorFR = null;
+    public DcMotor motorFL = null;
+    public DcMotor motorRR = null;
+    public DcMotor motorRL = null;
+    public DcMotor slide = null;
+    public DcMotor arm = null;
+    public DcMotor intake = null;
+    public Servo upleftClaw = null;//legacy code, can be removed
+    public Servo uprightClaw = null;//legacy code, can be removed
+
+    public static final double MID_SERVO = 0.5;//legacy code, can be removed
+    public ColorSensor colorSensor;//legacy code, can be removed
 
 
     /* local OpMode members. */
@@ -40,41 +41,49 @@ public class HardwareConfig {
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        frontRight = hwMap.get(DcMotor.class, "fr_motor");
-        frontLeft = hwMap.get(DcMotor.class, "fl_motor");
-        backRight = hwMap.get(DcMotor.class, "br_motor");
-        backLeft = hwMap.get(DcMotor.class, "bl_motor");
-        rackpinion = hwMap.get(DcMotor.class, "platform");
-        colorSensor = hwMap.colorSensor.get("color");
+        motorFR = hwMap.get(DcMotor.class, "motorFR");
+        motorFL = hwMap.get(DcMotor.class, "motorFL");
+        motorRR = hwMap.get(DcMotor.class, "motorRR");
+        motorRL = hwMap.get(DcMotor.class, "motorRL");
+        slide = hwMap.get(DcMotor.class, "slide");
+        arm = hwMap.get(DcMotor.class, "arm");
+        intake = hwMap.get(DcMotor.class, "intake");
+        colorSensor = hwMap.colorSensor.get("color");//legacy code, can be removed
 
 
-        upleftClaw = hwMap.get(Servo.class, "upleft_hand");
-        uprightClaw = hwMap.get(Servo.class, "upright_hand");
-        jewelhitter = hwMap.get(Servo.class, "jeweler");
+        upleftClaw = hwMap.get(Servo.class, "upleft_hand");//legacy code, can be removed
+        uprightClaw = hwMap.get(Servo.class, "upright_hand");//legacy code, can be removed
 
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        backLeft.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        frontRight.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        backRight.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        rackpinion.setDirection(DcMotor.Direction.REVERSE);
+        motorFR.setDirection(DcMotor.Direction.REVERSE);// Set to REVERSE if using AndyMark motors
+        motorFL.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        motorRR.setDirection(DcMotor.Direction.REVERSE);// Set to REVERSE if using AndyMark motors
+        motorRL.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        slide.setDirection(DcMotor.Direction.FORWARD);//requires testing
+        arm.setDirection(DcMotor.Direction.FORWARD);//requires testing
+        intake.setDirection(DcMotor.Direction.FORWARD);//requires testing
 
 
         // Set all motors to zero power
-        frontRight.setPower(0);
-        frontLeft.setPower(0);
-        backRight.setPower(0);
-        backLeft.setPower(0);
-        rackpinion.setPower(0);
+        motorFR.setPower(0);
+        motorFL.setPower(0);
+        motorRR.setPower(0);
+        motorRL.setPower(0);
+        slide.setPower(0);
+        arm.setPower(0);
+        intake.setPower(0);
 
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rackpinion.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //Not using encoders for non drive train to allow for more direct control of power.
 
 
     }
